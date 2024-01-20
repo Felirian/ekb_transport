fb1c9ae7a84f2e5712557ddb52e522df61d84c4e
Felirian <141245468+Felirian@users.noreply.github.com>
2024-01-20 03:13:35 +0300

    add: media sticky

--- src/components/TramSlider.js
+++ src/components/TramSlider.js
@@ -4,6 +4,8 @@ import img1 from '../assets/tramSlider/25.png'
 import img2 from '../assets/tramSlider/26.png'
 import img3 from '../assets/tramSlider/25.png'
 import styled from "styled-components";
+import {P1} from "../shared/styles";
+
 const TramSlider = () => {
   const [tramSel, setTramSel] = useState(0)
 
@@ -12,16 +14,16 @@ const TramSlider = () => {
   }, []);
 
   return (
-    <div>
+    <div style={{width: '100%'}}>
       <button onClick={() => setTramSel(0)}>1</button>
       <button onClick={() => setTramSel(1)}>2</button>
       <button onClick={() => setTramSel(2)}>3</button>
       <button onClick={() => setTramSel(3)}>4</button>
       <ImegeWrapper>
-        <Image src={img0} alt={''} style={{opacity: tramSel === 0 ? 1: 0}}/>
-        <Image src={img1} alt={''} style={{opacity: tramSel === 1 ? 1: 0}}/>
-        <Image src={img2} alt={''} style={{opacity: tramSel === 2 ? 1: 0}}/>
-        <Image src={img3} alt={''} style={{opacity: tramSel === 3 ? 1: 0}}/>
+        <Image src={img0} alt={''} style={{opacity: tramSel === 0 ? 1 : 0}}/>
+        <Image src={img1} alt={''} style={{opacity: tramSel === 1 ? 1 : 0}}/>
+        <Image src={img2} alt={''} style={{opacity: tramSel === 2 ? 1 : 0}}/>
+        <Image src={img3} alt={''} style={{opacity: tramSel === 3 ? 1 : 0}}/>
       </ImegeWrapper>
 
     </div>
--- src/pages/Media.js
+++ src/pages/Media.js
@@ -1,14 +1,71 @@
 import React from 'react';
 import Cards from "../components/Cards";
 import TramSlider from "../components/TramSlider";
-import {Container} from "../shared/styles";
+import {Container, H1, P1, P2} from "../shared/styles";
+import card1 from '../assets/cards/1.1.png';
+import card2 from '../assets/cards/1.2.png';
+import Sticky from "../components/Sticky";
+import {COLOR} from "../shared/variables";
 
 const Media = () => {
   return (
-    <Container>
-      <TramSlider/>
-      <Cards/>
+    <Container style={{display: "flex", flexDirection: "column", alignItems: "center", gap: '150px'}}>
+      <div style={{display: "flex", flexDirection: "column", alignItems: "center", gap: '60px'}}>
+        <TramSlider/>
+        <P1 style={{maxWidth: '1000px'}}>
+          Стилеобразующий элемент трамвая наносится только на декоративные панели
+          в верхней части вагона и нижних бортах. Цвет деталей передней и задней части вагона соответствует цветовой
+          палитре транспорта
+        </P1>
+      </div>
 
+      <H1 style={{color: COLOR.green}}>Проездные</H1>
+
+      <Sticky
+        height={'926px'}
+        text={
+          <>
+            <P2>
+              Для проездных карт использован шаблон визитной карточки с закругленными углами. На лицевой стороне
+              расположен паттерн и логотип бренда
+              <br/><br/>
+              На оборотной стороне, в верхней секции расположено название компании. В средней секции расположены
+              название тарифа, ФИО клиента и значки общественного транспорта, на которых клиент, согласно тарифу, может
+              ездить. В нижней секции расположен адрес веб-сайта компании и номер карты
+              <br/><br/>
+              Цвета для проездной карты используются соответственно цветовой палитре и принадлежности к виду транспорта
+              <br/><br/>
+              Формат 55х85 мм, стандарт
+            </P2>
+          </>
+        }
+        pos={'left'}
+        img={card1}
+      />
+
+      <Sticky
+        height={'926px'}
+        text={
+          <>
+            <P2>
+              Формат 55х85 мм, стандарт
+              <br/><br/>
+              На оборотной стороне, в верхней секции расположено название компании. В средней секции расположены
+              название тарифа и значок общественного транспорта, к которому принадлежит проездной. В нижней секции
+              расположен адрес веб-сайта компании и номер карты
+              <br/><br/>
+              Цвета для проездной карты используются соответственно цветовой палитре и принадлежности к виду транспорта
+            </P2>
+          </>
+        }
+        pos={'right'}
+        img={card2}
+      />
+
+      <H1 style={{color: COLOR.green}}>Сувениры</H1>
+
+
+      <Cards/>
     </Container>
   );
 };
