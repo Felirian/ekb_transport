470b6b60bd8ad5127dc43c8aee378be089cec9ee
Felirian <141245468+Felirian@users.noreply.github.com>
2024-01-20 05:40:29 +0300

    add: renders

--- src/pages/Media.js
+++ src/pages/Media.js
@@ -5,8 +5,21 @@ import {Container, H1, P1, P2} from "../shared/styles";
 
 import bigImg from '../assets/fullPage.png'
 
+import suv from '../assets/suvenirs.png'
+
+import tabloid from '../assets/tabloid.png'
+import tabloid1 from '../assets/tabloid1.png'
+import tabloid2 from '../assets/tabloid2.png'
+import tabloid3 from '../assets/tabloid3.png'
+
+import train from '../assets/train.png'
+
+import site from '../assets/site.png'
+
 import card1 from '../assets/cards/1.1.png';
 import card2 from '../assets/cards/1.2.png';
+
+
 import Sticky from "../components/Sticky";
 import {COLOR} from "../shared/variables";
 import styled from "styled-components";
@@ -23,10 +36,59 @@ const Media = () => {
         </P1>
       </div>
 
+      <div style={{display: "flex", gap: '60px'}}>
+        <img src={tabloid} alt={''} width={'345px'} height={'478px'} data-aos={"fade-right"}/>
+        <img src={tabloid1} alt={''} width={'345px'} height={'478px'} data-aos={"fade-left"}/>
+      </div>
+      <Text data-aos={"fade-up"}>
+        <P1>
+          Наклейки на стекло содержат фирменные цвета (акцентный цвет зависит от вида транспорта) и шрифты бренда -
+          Montserrat Light или Montserrat Regular
+          <br/><br/>
+          На наклейке крупно обозначен маршрут транспорта, стоимость проезда, а также возможность оплачивать проезд из
+          приложения в данном транспорте
+        </P1>
+      </Text>
+      <img src={tabloid2} alt={''} width={'998px'} height={'315px'} data-aos={"fade-left"}/>
+      <Text data-aos={"fade-up"}>
+        <P1>
+          На наклейке обозначены важные остановки фирменным шрифтом Montserrat Regular, остальные - Montserrat Light.
+          Это сделано для того, чтобы можно было прочитать информацию с наклейки на расстоянии до 25 метров
+        </P1>
+      </Text>
+
+      <img src={tabloid3} alt={''} height={'987px'} data-aos={"fade-up"}/>
+      <Text data-aos={"fade-up"}>
+        <P1>
+          Наклейки имеют размеры 250 мм × 370 мм и 1000 мм × 250 мм и располагаются от краев стекла на растоянии 40 мм.
+          Белая часть на макете обозначает полупрозрачную матовую часть наклейки. Линия маршрута на карте имеет тот же
+          цвет, что и транспорт
+        </P1>
+      </Text>
+
+      <img src={train} alt={''} data-aos={"fade-up"} height={'792px'}/>
+      <Text data-aos={"fade-up"}>
+        <P1>
+          На передней части обшивки трамвайного вагона изображен логотип суббренда в фирменном цвете. Он расположен по
+          центру, на расстоянии 80 мм от нижнего края лобового стекла и имеет размер 250 мм × 250 мм
+        </P1>
+      </Text>
+
+      <img src={site} alt={''} data-aos={"fade-up"} height={'490px'}/>
+
+      <Text data-aos={"fade-up"}>
+        <P1>
+          Для сидений трамвая используется тот же фирменный цвет, что и на панелях обшивки трамвая, с добавлением
+          фирменного паттерна бренда
+          <br/><br/><br/><br/><br/><br/>
+          Представляем вашему вниманию пример расположения компонентов на модели трамвая и то, как это могло бы
+          выглядеть в реальной жизни
+        </P1>
+      </Text>
+
       <img src={bigImg} alt={''} width={'1903px'} height={'7508px'}/>
 
       <H1 style={{color: COLOR.green}}>Проездные</H1>
-
       <Sticky
         height={'926px'}
         text={
@@ -67,17 +129,17 @@ const Media = () => {
         pos={'right'}
         img={card2}
       />
-
-      <H1 style={{color: COLOR.green}}>Сувениры</H1>
-
-
       <Cards/>
+      <H1 style={{color: COLOR.green}}>Сувениры</H1>
+      <img src={suv} alt={''}/>
+
+
     </Container>
   );
 };
 
 const Text = styled.div`
-    max-width: 100px;
+    max-width: 1000px;
     width: 100%;
 `
 
