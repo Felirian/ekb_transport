8411a9b07a6574f69bab5f095bd0abf20bc39278
Felirian <141245468+Felirian@users.noreply.github.com>
2024-01-20 06:24:18 +0300

    fix tram

--- src/components/TramSlider.js
+++ src/components/TramSlider.js
@@ -2,9 +2,9 @@ import React, {useEffect, useState} from 'react';
 import img0 from '../assets/tramSlider/24.png'
 import img1 from '../assets/tramSlider/25.png'
 import img2 from '../assets/tramSlider/26.png'
-import img3 from '../assets/tramSlider/25.png'
+import img3 from '../assets/tramSlider/27.png'
 import styled from "styled-components";
-import {P1} from "../shared/styles";
+import {HoverLink, P1} from "../shared/styles";
 
 const TramSlider = () => {
   const [tramSel, setTramSel] = useState(0)
@@ -14,17 +14,22 @@ const TramSlider = () => {
   }, []);
 
   return (
-    <div style={{width: '100%'}}>
-      <button onClick={() => setTramSel(0)}>1</button>
-      <button onClick={() => setTramSel(1)}>2</button>
-      <button onClick={() => setTramSel(2)}>3</button>
-      <button onClick={() => setTramSel(3)}>4</button>
+    <div style={{width: '100%', marginTop: '150px'}}>
+
       <ImegeWrapper>
         <Image src={img0} alt={''} style={{opacity: tramSel === 0 ? 1 : 0}}/>
         <Image src={img1} alt={''} style={{opacity: tramSel === 1 ? 1 : 0}}/>
         <Image src={img2} alt={''} style={{opacity: tramSel === 2 ? 1 : 0}}/>
         <Image src={img3} alt={''} style={{opacity: tramSel === 3 ? 1 : 0}}/>
       </ImegeWrapper>
+      <div style={{width: "fit-content"}}>
+        <HoverLink onClick={() => setTramSel(0)}>Итог</HoverLink>
+        <HoverLink onClick={() => setTramSel(1)}>Размеры</HoverLink>
+        <HoverLink onClick={() => setTramSel(2)}>Размещение</HoverLink>
+        <HoverLink onClick={() => setTramSel(3)}>Паттерн</HoverLink>
+
+      </div>
+
     </div>
   );
 };
